37f14d00d06b635b2a5b6915f7f2d57441672607
Meelyn Pandit <meelyn.pandit@gmail.com>
2025-01-28 16:23:26 -0500

    finally got duckdb to work, outpath variable needs database name

--- R/workshop_API.R
+++ R/workshop_API.R
@@ -10,28 +10,28 @@ start <- Sys.time()
 
 ### Install DuckDB from R Universe ###
 # install.packages("duckdb", repos = c("https://duckdb.r-universe.dev", "https://cloud.r-project.org"))
+# install.packages("https://github.com/duckdb/duckdb/releases/download/master-builds/duckdb_r_src.tar.gz", repos = NULL)
 
 ####SETTINGS#####
 myproject <- "Meadows V2" #this is your project name on your CTT account
-outpath <- "../vignettes/aos2024/" #where your downloaded files are to go
+outpath <- "./vignettes/aos2024/" #where your downloaded files are to go
 my_token <- "c2ed5f935e9b9d4c2e031f8a96277317b7502d989add5947656dbfbeee7082c5"
-# my_token <- '49a778c42209d6ab5c0145acc6c3850addd272fd373e8430c7dd40638843921e'
+
 
 con <- DBI::dbConnect(
-  duckdb::duckdb(), 
-  # dbdir = '../vignettes/aos2024',
-  # dbdir = './',
+  duckdb::duckdb(),
+  dbdir = "./vignettes/aos2024/meadows.db",
   read_only = FALSE
 )
 
 ################
 get_my_data(
-  my_token, 
-  outpath, 
-  con, 
-  myproject=myproject, 
-  begin=as.Date("2024-08-01"), 
-  end=as.Date("2024-08-03"), 
+  my_token,
+  outpath,
+  con,
+  myproject=myproject,
+  begin=as.Date("2025-01-01"),
+  end=as.Date("2025-01-03"),
   filetypes=c("raw", "node_health")
 )
 
@@ -42,3 +42,17 @@ time_elapse <- Sys.time() - start
 print(time_elapse)
 
 #raw <- tbl(con, "node_health") |> collect()
+
+
+# Import Node Data --------------------------------------------------------
+con <- DBI::dbConnect(
+  duckdb::duckdb(),
+  dbdir = "./vignettes/aos2024/meadows.db",
+  read_only = FALSE
+)
+
+
+import_node_data(con,
+                 outpath = outpath,
+                 myproject="Meadows V2")
+
