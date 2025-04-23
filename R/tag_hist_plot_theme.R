f330955a7cd451753af06335020e1683effcb9d0
Meelyn Pandit <meelyn.pandit@gmail.com>
2025-04-23 13:12:22 -0400

    change tag_hist_plot_theme into a function

--- R/tag_hist_plot_theme.R
+++ R/tag_hist_plot_theme.R
@@ -1,18 +1,34 @@
 library(ggplot2)
 
-tag_hist_plot_theme <- theme(
-  # axis.text.x = element_text(size = 20, vjust = 0.5),
-  axis.text.y = element_text(family = "courier"),
-  # axis.title.x = element_text(size = 20, vjust = 0.5),
-  # axis.title.y = element_text(size = 20, vjust = 0.5),
-  axis.line = element_line(colour = "black", linewidth = 0.5),
-  panel.grid.major = element_line(colour = "grey", linewidth = 0.25),
-  panel.grid.minor = element_line(colour = "grey", linewidth = 0.1),
-  panel.background = element_rect(fill = "white"),
-  plot.margin = unit(c(1, 1, 0.5, 0.5), "inches"),
-  plot.title = element_text(
-    size = 25,
-    face = "bold",
-    hjust = 0.5
-  ),
-)
+
+#' Title
+#'
+#' @param base_size
+#' @param base_family
+#'
+#' @returns
+#' @export
+#'
+#' @examples
+tag_hist_plot_theme <- function(
+  base_size = 12,
+  base_family = 'sans') {
+    (theme_foundation(base_size = base_size,
+                      base_family = base_family)) +
+    theme(
+    # axis.text.x = element_text(size = 20, vjust = 0.5),
+    axis.text.y = element_text(family = "courier"),
+    # axis.title.x = element_text(size = 20, vjust = 0.5),
+    # axis.title.y = element_text(size = 20, vjust = 0.5),
+    axis.line = element_line(colour = "black", linewidth = 0.5),
+    panel.grid.major = element_line(colour = "grey", linewidth = 0.25),
+    panel.grid.minor = element_line(colour = "grey", linewidth = 0.1),
+    panel.background = element_rect(fill = "white"),
+    plot.margin = unit(c(1, 1, 0.5, 0.5), "inches"),
+    plot.title = element_text(
+      size = 25,
+      face = "bold",
+      hjust = 0.5
+    ),
+  )
+}
