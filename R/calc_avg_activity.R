fd57302ff72740b4602829335ebacc9f4750d6cf
Meelyn Pandit <meelyn.pandit@gmail.com>
2025-04-28 08:18:19 -0400

    add calc_avg_activity()

---
+++ R/calc_avg_activity.R
@@ -0,0 +1,38 @@
+#' Title
+#'
+#' @param tag_activity
+#' @param start_time
+#' @param stop_time
+#'
+#' @returns
+#' @export
+#'
+#' @examples
+calc_avg_activity <- function(tag_activity, start_time, stop_time) {
+  result <- data.frame(
+    time = as.POSIXct(character()),
+    avg_activity = double(),
+    sd_activity = double()
+  )
+
+  t <- start_time
+  t_bin_size <- 3600
+  while (t < stop_time) {
+    time_range_data <- subset.data.frame(tag_activity, tag_activity$time >= t)
+    time_range_data <- subset.data.frame(time_range_data, time_range_data$time < t + t_bin_size)
+
+    avg <- mean(time_range_data$abs_act)
+    sd <- sd(time_range_data$abs_act)
+
+    df <- data.frame(
+      time = t + t_bin_size / 2,
+      avg_activity = avg,
+      sd_activity = sd
+    )
+    result <- rbind(result, df)
+
+    t <- t + t_bin_size
+  }
+
+  return(result)
+}
