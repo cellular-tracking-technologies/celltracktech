013d2722b39579d821966baa1dee32cc4cb0b7c8
Meelyn Pandit <meelyn.pandit@gmail.com>
2025-04-29 17:05:58 -0400

    test data.R

--- R/data.R
+++ R/data.R
@@ -2,7 +2,7 @@
 #'
 #' An example of the sidekick calibration in the Meadows V2 field site.
 #'
-#' @format A data frame with 716 rows and 8 columns:
+#' @format ## `sidekick_cal` A data frame with 716 rows and 8 columns:
 #' \describe{
 #'   \item{tag_type}{Tag type detected}
 #'   \item{tag_id}{Tag ID}
@@ -14,3 +14,23 @@
 #'   \item{antenna_angle}{Angle of antenna}
 #' }
 "sidekick_cal"
+
+#' Another sidekick calibration file
+#'
+#' A subset of data from the World Health Organization Global Tuberculosis
+#' Report ...
+#'
+#' @format ## `sidekick_all_df`
+#' A data frame with 7,240 rows and 60 columns:
+#' @format A data frame with 716 rows and 8 columns:
+#' \describe{
+#'   \item{tag_type}{Tag type detected}
+#'   \item{tag_id}{Tag ID}
+#'   \item{time_utc}{Time in UTC}
+#'   \item{rssi}{Received signal strength indicator in dBm}
+#'   \item{lat}{Latitude}
+#'   \item{lon}{Longitude}
+#'   \item{heading}{Cardinal direction}
+#'   \item{antenna_angle}{Angle of antenna}
+#' }
+"sidekick_all_df"
