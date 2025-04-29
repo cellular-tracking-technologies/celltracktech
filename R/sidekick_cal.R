6593b910b67b4d8eb68385f6f0355cc86036e8ec
Meelyn Pandit <meelyn.pandit@gmail.com>
2025-04-29 14:05:22 -0400

    add sidekick_cal to R

---
+++ R/sidekick_cal.R
@@ -0,0 +1,18 @@
+#' Sidekick Calibration Data
+#'
+#' An example of the sidekick calibration in the Meadows V2 field site.
+#'
+#' @format ## `sidekick_cal`
+#' A data frame with 716 rows and 8 columns:
+#' \describe{
+#'   \item{tag_type}{Tag type detected}
+#'   \item{tag_id}{Tag ID}
+#'   \item{time_utc}{Time in UTC}
+#'   \item{rssi}{Received signal strength indicator in dBm}
+#'   \item{lat}{Latitude}
+#'   \item{lon}{Longitude}
+#'   \item{heading}{Cardinal direction}
+#'   \item{antenna_angle}{Angle of antenna}
+#' }
+#'
+"sidekick_cal"
