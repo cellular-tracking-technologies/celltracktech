981156b18976cd69e8ed1d1eacada735924bcee9
Meelyn Pandit <meelyn.pandit@gmail.com>
2025-04-30 11:15:58 -0400

    add deployments dataframe

--- R/data.R
+++ R/data.R
@@ -15,22 +15,17 @@
 #' }
 "sidekick_cal"
 
-#' Another sidekick calibration file
+#' Meadows Deployment Data
 #'
-#' A subset of data from the World Health Organization Global Tuberculosis
-#' Report ...
+#' Tag deployment data for the Meadows V2 project
 #'
-#' @format ## `sidekick_all_df`
-#' A data frame with 7,240 rows and 60 columns:
-#' @format A data frame with 716 rows and 8 columns:
+#' @format ## `deployments` A data frame with 125 rows and 5 columns:
+#'
 #' \describe{
-#'   \item{tag_type}{Tag type detected}
-#'   \item{tag_id}{Tag ID}
-#'   \item{time_utc}{Time in UTC}
-#'   \item{rssi}{Received signal strength indicator in dBm}
-#'   \item{lat}{Latitude}
-#'   \item{lon}{Longitude}
-#'   \item{heading}{Cardinal direction}
-#'   \item{antenna_angle}{Angle of antenna}
+#'   \item{TagId}{Tag ID}
+#'   \item{DeployDate}{Date tag was deployed in the field}
+#'   \item{Species}{Alpha-numeric code for species}
+#'   \item{TagType}{Tag Type: Power, Hybride, Life, Blu}
+#'   \item{AntennaType}{1/8 wave or 1/4 wave}
 #' }
-"sidekick_all_df"
+"deployments"
