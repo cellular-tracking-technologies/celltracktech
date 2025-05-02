1fdafe3ad127c2c02aace34e6b07046cfaa39670
Meelyn Pandit <meelyn.pandit@gmail.com>
2025-05-02 15:53:39 -0400

    add sidekick calibration test files to package

--- R/data.R
+++ R/data.R
@@ -15,6 +15,57 @@
 #' }
 "sidekick_cal"
 
+#' Sidekick Calibration Test Data 1
+#'
+#' An example of the sidekick test calibration in the Meadows V2 field site.
+#'
+#' @format ## `sidekick_cal_test1` A data frame with 93 rows and 8 columns:
+#' \describe{
+#'   \item{tag_type}{Tag type detected}
+#'   \item{tag_id}{Tag ID}
+#'   \item{time_utc}{Time in UTC}
+#'   \item{rssi}{Received signal strength indicator in dBm}
+#'   \item{lat}{Latitude}
+#'   \item{lon}{Longitude}
+#'   \item{heading}{Cardinal direction}
+#'   \item{antenna_angle}{Angle of antenna}
+#' }
+"sidekick_cal_test1"
+
+#' Sidekick Calibration Test Data 2
+#'
+#' An example of the sidekick test calibration in the Meadows V2 field site.
+#'
+#' @format ## `sidekick_cal_test2` A data frame with 415 rows and 8 columns:
+#' \describe{
+#'   \item{tag_type}{Tag type detected}
+#'   \item{tag_id}{Tag ID}
+#'   \item{time_utc}{Time in UTC}
+#'   \item{rssi}{Received signal strength indicator in dBm}
+#'   \item{lat}{Latitude}
+#'   \item{lon}{Longitude}
+#'   \item{heading}{Cardinal direction}
+#'   \item{antenna_angle}{Angle of antenna}
+#' }
+"sidekick_cal_test2"
+
+#' Sidekick Calibration Test Data 3
+#'
+#' An example of the sidekick test calibration in the Meadows V2 field site.
+#'
+#' @format ## `sidekick_cal_test3` A data frame with 415 rows and 8 columns:
+#' \describe{
+#'   \item{tag_type}{Tag type detected}
+#'   \item{tag_id}{Tag ID}
+#'   \item{time_utc}{Time in UTC}
+#'   \item{rssi}{Received signal strength indicator in dBm}
+#'   \item{lat}{Latitude}
+#'   \item{lon}{Longitude}
+#'   \item{heading}{Cardinal direction}
+#'   \item{antenna_angle}{Angle of antenna}
+#' }
+"sidekick_cal_test3"
+
 #' Meadows Deployment Data
 #'
 #' Tag deployment data for the Meadows V2 project
