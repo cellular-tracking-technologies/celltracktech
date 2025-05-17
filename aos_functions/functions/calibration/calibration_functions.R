48aa827a0562571a20c23cce305e4df784e5ed36
Meelyn Pandit <meelyn.pandit@gmail.com>
2025-05-16 20:07:47 -0400

    remove aos_functions and workshop_api

--- aos_functions/functions/calibration/calibration_functions.R
+++
@@ -1,3 +0,0 @@
-source("R/functions/calibration/map_calibration_track.R")
-source("R/functions/calibration/calc_rssi_v_dist.R")
-source("R/functions/calibration/plot_calibration_result.R")
--- aos_functions/functions/tag/tag_functions.R
+++
@@ -1,4 +0,0 @@
-# source("R/functions/tag/load_node_detection_data.R")
-source("R/functions/tag/get_tag_detections.R")
-source("R/functions/tag/detection_summary.R")
-source("R/functions/tag/calculate_tag_activity.R")
